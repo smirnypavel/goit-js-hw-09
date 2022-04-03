30ad7a8dc759080f5fcb21efd7ab26c8e2d992de
smirnypavel <smirnypavlik@gmail.com>
2022-04-03 12:32:50 +0300

    1 hw

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,24 @@
 
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
+// const spanEl = document.querySelector('.color');
+const buttonStart = document.querySelector('button[data-start]');
+const buttonStop = document.querySelector('button[data-stop]');
+
+
+
+buttonStart.addEventListener('click', () => {
+    timerId = setInterval(() => {
+        const color = getRandomHexColor();
+        document.body.style.background = color;;
+    }, 1000);
+    buttonStart.setAttribute('disabled','disabled');
+    // console.log(buttonStart);
+});
+
+buttonStop.addEventListener("click", () => {
+    clearInterval(timerId);
+    buttonStart.removeAttribute('disabled');
+});
+
